2697b2c9953ccc545c567007679658886b541ffc
Aaliyah <justliya@icloud.com>
2025-08-05 12:32:29 -0500

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cross-Origin-Embedder-Policy", value: "credentialless" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Permissions-Policy", value: "web-share=(self)" }, // ✅ Add this line
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
